a5e69caacd836edba7e068731304eec2eb0a4152
imran0347 <104224003+imran0347@users.noreply.github.com>
2025-06-28 19:13:35 +0530

    Update hello_cdk_stack.py

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
   Stack,
+  aws_lambda as _lambda, # Import the Lambda module
 )
 from constructs import Construct
 
@@ -8,4 +9,19 @@ class HelloCdkStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
-    # Define your constructs here
+    # Define the Lambda function resource
+    my_function = _lambda.Function(
+      self, "HelloWorldFunction",
+      runtime = _lambda.Runtime.NODEJS_20_X, # Provide any supported Node.js runtime
+      handler = "index.handler",
+      code = _lambda.Code.from_inline(
+        """
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World!'),
+          };
+        };
+        """
+      ),
+    )

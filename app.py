2e240d4badf905578a3be21716579a16250ef03e
imran0347 <104224003+imran0347@users.noreply.github.com>
2025-06-28 19:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import aws_cdk as cdk
 from hello_cdk.hello_cdk_stack import HelloCdkStack
 
 app = cdk.App()
-HelloCdkStack(app, "HelloCdkStack",)
+HelloCdkStack(app, "HelloCdkStack",
+  env=cdk.Environment(account='942962191510', region='us-east-1'),
+  )
 
 app.synth()
